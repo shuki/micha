1ea1c1984a7e03df819b6b3c01e0a1df7d372750
Shuki Shukrun <shukrun.shuki@gmail.com>
2014-09-15 18:57:16 +0300

    add event_type

--- jset/server/class/micha.class.php
+++ jset/server/class/micha.class.php
@@ -9,5 +9,14 @@ class micha
 		$db->query("select if(max(tid) is null, 1 , max(tid) + 1) as result from jset_list where `type` = 'area'");
 		return $db->fetch()->result;
 	}
+
+	public static function next_event_type_tid($db, $tid)
+	{
+		if($tid)
+			return $tid;
+		
+		$db->query("select if(max(tid) is null, 1 , max(tid) + 1) as result from jset_list where `type` = 'event_type'");
+		return $db->fetch()->result;
+	}
 }
 ?>
